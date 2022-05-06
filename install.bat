212a81e1c3e4fb437250636d966b419a1a8712d1
montesr <ruggero.montesi@dematic.com>
2022-05-06 23:17:46 +0300

    formatting

--- install.bat
+++ install.bat
@@ -1,14 +1,14 @@
 call git clone  https://github.com/ruggeromontesi/exam-validator.git
 cd  exam-validator
 call mvn clean install -f pom.xml
-call mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"
-REM mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"  -Dexec.args="8"
+REM call mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"
+mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"  -Dexec.args="11"
 cd target/generated-reports
 start notepad aggregated-report.txt
 start threshold-based-report.txt
 start notepad detailed-report.txt
 
-cmd /k
+REM cmd /k
 
 
 REM if the user wants to pass a different threshold to determine the cheating students
