39bf73b87a40717164ed2410d87276d7a0d44fd6
montesr <ruggero.montesi@dematic.com>
2022-05-06 23:24:56 +0300

    modified install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@ call git clone  https://github.com/ruggeromontesi/exam-validator.git
 cd  exam-validator
 call mvn clean install -f pom.xml
 REM call mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"
-mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"  -Dexec.args="11"
+call mvn compile exec:java -Dexec.mainClass="com.exam.validator.Main"  -Dexec.args="11"
 cd target/generated-reports
 start notepad aggregated-report.txt
 start threshold-based-report.txt
