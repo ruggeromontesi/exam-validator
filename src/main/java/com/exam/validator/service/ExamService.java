205f90f74dce5b605b367f0d28ff02cff8fdbb1e
montesr <ruggero.montesi@dematic.com>
2022-05-14 16:09:03 +0300

    Exam class replaced by examService

--- src/main/java/com/exam/validator/service/ExamService.java
+++ src/main/java/com/exam/validator/service/ExamService.java
@@ -4,29 +4,45 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 import com.exam.validator.constants.Constants;
-import com.exam.validator.entity.Exam;
 import com.exam.validator.entity.Student;
+import com.exam.validator.util.CSVReader;
 
 public class ExamService {
 
-   private final Exam exam;
+   private int cheatingThreshold;
 
-   /**Accepts
-    * @param args To be passed to Exam constructor.
+   private final List<Student> studentList = CSVReader.parse();
+
+   /**Accepts a vararg parameter of type String. If no or invalid String is passed a predefined threshold is used.
+    * @param args A String containing an integer number representing the threshold for determining if the student has cheated.
+    * If the students has a number of answers identical to a neighbour he is considered to have cheated.
     * */
-
    public ExamService(String... args) {
-      this.exam = new Exam(args);
-      File directory = new File(Constants.REPORTS_DIRECTORY);
+       File directory = new File(Constants.REPORTS_DIRECTORY);
       if (!directory.exists()) {
          directory.mkdirs();
+      }
+
+      if (args.length == 0) {
+         cheatingThreshold = Constants.CHEATING_THRESHOLD;
+      } else {
+         try {
+            int userThreshold = Integer.parseInt(args[0]);
+            cheatingThreshold = (userThreshold > 0 && userThreshold < 17) ? userThreshold : Constants.CHEATING_THRESHOLD;
+         } catch (NumberFormatException ex) {
+            cheatingThreshold = Constants.CHEATING_THRESHOLD;
+         }
       }
    }
 
@@ -36,20 +52,20 @@ public class ExamService {
     * @throws IOException if path is not found
     */
    public void printAggregatedReport() throws IOException {
-      Map<Student, Map<Student,Integer>> report = new TreeMap<>(exam.getStudentComparator());
-      exam.getStudentList().forEach(student -> report.put(student, exam.getAnswersComparisonForThisStudent(student)));
+      Map<Student, Map<Student,Integer>> report = new TreeMap<>(getStudentComparator());
+      getStudentList().forEach(student -> report.put(student, getAnswersComparisonForThisStudent(student)));
 
       BufferedWriter writer = new BufferedWriter(new FileWriter(Constants.REPORTS_DIRECTORY + "/"
             + Constants.AGGREGATED_REPORT_FILENAME));
       writer.write(Constants.AGGREGATED_REPORT_HEADER);
       StringBuilder reportLineSb = new StringBuilder("");
-      exam.getStudentList().forEach(student -> report.put(student, exam.getAnswersComparisonForThisStudent(student)));
+      getStudentList().forEach(student -> report.put(student, getAnswersComparisonForThisStudent(student)));
       report.forEach((key, value) -> {
-         int maximumNumberOfIdenticalQuestions = exam.getAnswersComparisonForThisStudent(key)
+         int maximumNumberOfIdenticalQuestions = getAnswersComparisonForThisStudent(key)
                .entrySet()
                .stream()
                .max(Comparator.comparingInt(Map.Entry::getValue)).get().getValue();
-         List<Map.Entry<Student, Integer>> maxEntryList = exam.getAnswersComparisonForThisStudent(key)
+         List<Map.Entry<Student, Integer>> maxEntryList = getAnswersComparisonForThisStudent(key)
                .entrySet()
                .stream()
                .filter(studentIntegerEntry -> studentIntegerEntry.getValue() == maximumNumberOfIdenticalQuestions)
@@ -73,9 +89,9 @@ public class ExamService {
             new BufferedWriter(new FileWriter(Constants.REPORTS_DIRECTORY + "/" + Constants.DETAILED_REPORT_FILENAME));
       writer.write(Constants.DETAILED_REPORT_HEADER);
       StringBuilder reportLineSb = new StringBuilder("");
-      exam.getStudentList().forEach(student -> {
+      getStudentList().forEach(student -> {
          reportLineSb.append("Comparison of identical answers for ").append(String.format("%-11.11s", student.getName()));
-         exam.getAnswersComparisonForThisStudent(student).forEach((key, value) -> {
+         getAnswersComparisonForThisStudent(student).forEach((key, value) -> {
             reportLineSb.append("  {").append(key.getName()).append(" : ").append(value).append("}\t");
          });
          reportLineSb.append("\n");
@@ -107,13 +123,144 @@ public class ExamService {
       BufferedWriter writer =
             new BufferedWriter(new FileWriter(Constants.REPORTS_DIRECTORY + "/" + Constants.THRESHOLD_BASED_REPORT_FILENAME));
       writer.write(Constants.THRESHOLD_BASED_REPORT_HEADER);
-      writer.write("The predefined threshold is equal to " + exam.getCheatingThreshold() + "\n\n\n");
+      writer.write("The predefined threshold is equal to " + getCheatingThreshold() + "\n\n\n");
       StringBuilder reportLineSb = new StringBuilder("");
-      exam.getStudentList().stream().filter(exam::isThisStudentCheating).collect(Collectors.toList()).forEach(student -> {
+      getStudentList().stream().filter(this::isThisStudentCheating).collect(Collectors.toList()).forEach(student -> {
          reportLineSb.append(student.toString()).append("\n");
       });
       writer.write(reportLineSb.toString());
       writer.close();
+   }
+
+   private final ToIntFunction<Student> studentToIntFunction = student -> {
+      Map<Student,Integer> singleStudentReport = new HashMap<>();
+      getNeighboursList(student).forEach(
+            neighbourStudent -> {
+               int amountOfIdenticalAnswers = (int) neighbourStudent
+                     .getAnswers()
+                     .entrySet()
+                     .stream()
+                     .filter(e -> e.getValue().equals(student.getAnswers().get(e.getKey()))).count();
+               singleStudentReport.put(neighbourStudent,amountOfIdenticalAnswers);
+            }
+      );
+      return singleStudentReport.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getValue();
+   };
+
+   private final Comparator<Student> studentComparator = Comparator.comparingInt(studentToIntFunction)
+         .reversed().thenComparing(Student::getName);
+
+   public Comparator<Student> getStudentComparator() {
+      return studentComparator;
+   }
+
+   public int getCheatingThreshold() {
+      return cheatingThreshold;
+   }
+
+   public List<Student> getStudentList() {
+      return studentList;
+   }
+
+   /**
+    * Given a coordinate it retrieves the Student sitting at this coordinate and wraps it within an Optional.
+    * @param coordinate the coordinate from which is required to get the relevant Student instance.
+    * @return the Student with sitting location equal to the given coordinate.
+    */
+   public Optional<Student> getByCoordinate(String coordinate) {
+      return studentList.stream().filter(
+            student -> student.getSittingLocation().equals(coordinate)
+      ).collect(
+            Collectors.collectingAndThen(
+                  Collectors.toList(),
+                  list -> {
+                     if (list.size() > 1) {
+                        throw new RuntimeException("More than a student with with same sitting location!");
+                     }
+                     return list.isEmpty() ? Optional.empty() : Optional.of(list.get(0));
+                  }
+            ));
+   }
+
+   /**
+    * Determines the list of coordinates of neighbouring positions for the Student passed as parameter .
+    * @param thisStudent The students for which is required to determine the neighbouring positions.
+    * @return the list of neighbouring positions.
+    */
+   public List<String> getCoordinatesOfNeighbouringSittingLocations(Student thisStudent) {
+      int rowIndex = Integer.parseInt(thisStudent.getSittingLocation().substring(0,1));
+      int colIndex = Integer.parseInt(thisStudent.getSittingLocation().substring(2,3));
+      List<String> possibleCoordinateOfNeighbours = new ArrayList<>();
+      //EAST neighbour
+      if (colIndex > 1) {
+         possibleCoordinateOfNeighbours.add(rowIndex + "." + (colIndex - 1));
+      }
+      //WEST neighbour
+      if (colIndex < Constants.AUDITORIUM_NUMBER_OF_COLUMNS) {
+         possibleCoordinateOfNeighbours.add(rowIndex + "." + (colIndex + 1));
+      }
+      //SOUTH-WEST neighbour
+      if (rowIndex > 1 && colIndex > 1) {
+         possibleCoordinateOfNeighbours.add((rowIndex - 1) + "." + (colIndex - 1));
+      }
+      //SOUTH neighbour
+      if (rowIndex > 1) {
+         possibleCoordinateOfNeighbours.add((rowIndex - 1) + "." + colIndex);
+      }
+      //SOUTH-EAST neighbour
+      if (rowIndex > 1 && colIndex < Constants.AUDITORIUM_NUMBER_OF_COLUMNS) {
+         possibleCoordinateOfNeighbours.add((rowIndex - 1) + "." + (colIndex + 1));
+      }
+      return possibleCoordinateOfNeighbours;
+   }
+
+   /**
+    * Determines the list of  students neighbouring to the student passed as parameter.
+    * @param thisStudent the student for which is determined the list of neighbours.
+    * @return the list of neighbours.
+    */
+   public List<Student> getNeighboursList(Student thisStudent) {
+      List<String> possibleCoordinateOfNeighbours = getCoordinatesOfNeighbouringSittingLocations(thisStudent);
+      return possibleCoordinateOfNeighbours
+            .stream()
+            .map(this::getByCoordinate)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toList());
+   }
+
+   /**
+    * Determines if the student has cheated based on the amount of answers identical to his neighbours.
+    * @param thisStudent the student for which is required to determine if he has cheated or not.
+    * @return if this student has cheated or not.
+    */
+   public boolean isThisStudentCheating(Student thisStudent) {
+      List<Student> neighboursList = getNeighboursList(thisStudent);
+      return neighboursList
+            .stream().anyMatch(student -> student
+                  .getAnswers()
+                  .entrySet()
+                  .stream()
+                  .filter(e -> e.getValue().equals(thisStudent.getAnswers().get(e.getKey()))).count() > cheatingThreshold);
+   }
+
+   /**Creates a map where keys are the neighbouring students and values the amount fo identical answers.
+    * @param thisStudent student for which the map is generated
+    * @return map Student/ amount of identical values
+    */
+   public Map<Student,Integer> getAnswersComparisonForThisStudent(Student thisStudent) {
+      Map<Student,Integer> singleStudentReport = new HashMap<>();
+      getNeighboursList(thisStudent).forEach(
+            neighbourStudent -> {
+               int amountOfIdenticalAnswers = (int) neighbourStudent
+                     .getAnswers()
+                     .entrySet()
+                     .stream()
+                     .filter(e -> e.getValue().equals(thisStudent.getAnswers().get(e.getKey()))).count();
+               singleStudentReport.put(neighbourStudent,amountOfIdenticalAnswers);
+            }
+      );
+      return singleStudentReport;
    }
 
 }
--- src/test/java/com/exam/validator/entity/ExamServiceTest.java
+++ src/test/java/com/exam/validator/entity/ExamServiceTest.java
@@ -6,24 +6,26 @@ import org.junit.Test;
 
 import java.util.*;
 
-public class ExamTest {
+import com.exam.validator.service.ExamService;
 
-   private Exam exam;
+public class ExamServiceTest {
+
+   private ExamService examService;
 
    @Before
    public void setUp() {
-      exam = new Exam();
+      examService = new ExamService();
    }
 
    @Test
    public void testCreateStudentList(){
-      Assert.assertFalse(exam.getStudentList().isEmpty());
+      Assert.assertFalse(examService.getStudentList().isEmpty());
    }
 
    @Test
    public void testGetByCoordinate(){
       String coordinateStudent29 = "4.5";
-      Assert.assertEquals("Studentas29", exam.getByCoordinate(coordinateStudent29).get().getName());
+      Assert.assertEquals("Studentas29", examService.getByCoordinate(coordinateStudent29).get().getName());
    }
 
    @Test
@@ -41,18 +43,18 @@ public class ExamTest {
               sittingLocationStudent13
       );
 
-      Student student12 = exam.getStudentList().get(11);
-      exam.getCoordinatesOfNeighbouringSittingLocations(student12).forEach(
+      Student student12 = examService.getStudentList().get(11);
+      examService.getCoordinatesOfNeighbouringSittingLocations(student12).forEach(
               coordinate ->
               Assert.assertTrue(coordinatesList.contains(coordinate))
       );
-      Assert.assertEquals(5,exam.getCoordinatesOfNeighbouringSittingLocations(student12).size());
+      Assert.assertEquals(5,examService.getCoordinatesOfNeighbouringSittingLocations(student12).size());
 
    }
 
    @Test
    public void testGetNeighboursList(){
-      List<Student> studentList = exam.getStudentList();
+      List<Student> studentList = examService.getStudentList();
       List<Student> neighbours = Arrays.asList(
               studentList.get(3),
               studentList.get(4),
@@ -64,31 +66,31 @@ public class ExamTest {
       Student student13 = studentList.get(12);
 
       Assert.assertEquals(neighbours.size(),
-            neighbours.stream().filter(student -> exam.getNeighboursList(student13).contains(student) ).count());
+            neighbours.stream().filter(student -> examService.getNeighboursList(student13).contains(student) ).count());
    }
 
    @Test
    public void testIsThisStudentCheating() {
-      Student student12 = exam.getStudentList().get(11);
-      Assert.assertTrue(exam.isThisStudentCheating(student12));
+      Student student12 = examService.getStudentList().get(11);
+      Assert.assertTrue(examService.isThisStudentCheating(student12));
    }
 
    @Test
    public void testGetAnswersComparisonForThisStudent(){
       Map<Student,Integer> mapForStudent1 = new HashMap<>();
-      mapForStudent1.put(exam.getStudentList().get(1),6);
-      Assert.assertEquals(mapForStudent1, exam.getAnswersComparisonForThisStudent(exam.getStudentList().get(0)));
+      mapForStudent1.put(examService.getStudentList().get(1),6);
+      Assert.assertEquals(mapForStudent1, examService.getAnswersComparisonForThisStudent(examService.getStudentList().get(0)));
    }
 
    @Test
    public void testComparator(){
-      List<Student> studentList = exam.getStudentList();
+      List<Student> studentList = examService.getStudentList();
       Student student1 = studentList.get(0);
       Student student6 = studentList.get(5);
-      Map<Student, Map<Student,Integer>> newOutputReports = new TreeMap<>(exam.getStudentComparator());
-      newOutputReports.put(student1,exam.getAnswersComparisonForThisStudent(student1) );
-      newOutputReports.put(student6,exam.getAnswersComparisonForThisStudent(student6) );
-      //student 6 has highest numeber of identical answers then should come first in the map.
+      Map<Student, Map<Student,Integer>> newOutputReports = new TreeMap<>(examService.getStudentComparator());
+      newOutputReports.put(student1,examService.getAnswersComparisonForThisStudent(student1) );
+      newOutputReports.put(student6,examService.getAnswersComparisonForThisStudent(student6) );
+      //student 6 has highest number of identical answers then should come first in the map.
       newOutputReports.entrySet().stream().limit(1).forEach(e -> Assert.assertEquals("Studentas6", e.getKey().getName()));
    }
 
